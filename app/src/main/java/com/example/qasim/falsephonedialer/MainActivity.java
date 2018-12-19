6247a5b97af40cca2d16d3622d2fd5a2c37ca8ae
Muhammad Qasim <qasim14@outlook.com>
2018-12-19 22:32:51 +0500

    a
    a

--- app/src/main/java/com/example/qasim/falsephonedialer/MainActivity.java
+++ app/src/main/java/com/example/qasim/falsephonedialer/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.qasim.falsephonedialer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvMessage=findViewById(R.id.tvMessage);
+
+        if(getIntent().getData()!=null)
+        {
+    tvMessage.setText(getIntent().getData().toString());
+        }
+        else
+        {
+        tvMessage.setText("No Data Receiveed");
+        }
+
     }
 }
